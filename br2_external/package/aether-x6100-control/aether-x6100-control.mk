309b199ef74792971d5f5a7025bebd1e4ec3baa3
Belousov Oleg <belousov.oleg@gmail.com>
2023-01-25 04:02:49 +0300

    Update x6100-control package

--- br2_external/package/aether-x6100-control/aether-x6100-control.mk
+++ br2_external/package/aether-x6100-control/aether-x6100-control.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AETHER_X6100_CONTROL_VERSION = 8f7fe65264fdc762f75da7faa4a17560cea89538
+AETHER_X6100_CONTROL_VERSION = v0.9.0
 AETHER_X6100_CONTROL_SOURCE = $(AETHER_X6100_CONTROL_VERSION).tar.gz
 AETHER_X6100_CONTROL_SITE = https://github.com/AetherRadio/X6100Control/archive
 AETHER_X6100_CONTROL_LICENSE = GPLv2
